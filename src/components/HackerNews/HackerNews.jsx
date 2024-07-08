1da8b5120eac1d90574675bccca0f4c65293e2e7
Scott Johnstone <scottajohnstone@gmail.com>
2024-07-08 14:19:00 -0500

    used the async function in HackerNews.jsx file and displayed the articles

--- src/components/HackerNews/HackerNews.jsx
+++ src/components/HackerNews/HackerNews.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Card from '../shared/Card/Card.jsx';
 import Loading from '../shared/Loading/Loading.jsx';
+import { requestArticles } from '../../reducers/hackerNewsReducer.js';
 
 export default function HackerNews() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const articles = useSelector((state) => state.articles);
+  const loading = useSelector((state) => state.loading);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(requestArticles);
+  }, [])
 
   const articleCards = articles.map((article) => <Card key={article.id} article={article} />);
   return (
